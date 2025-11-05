91ab223c81398628ac4ebe76a74981873a62128a
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-11-05 09:48:32 -0600

    Initial Camera Setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    cv2.imshow("Camara", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
